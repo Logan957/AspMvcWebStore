e942c7928a41155c0efc828d6a1cd978e30cd85b
Logan957 <roman.zaidulin2015@gmail.com>
2021-04-21 15:56:29 +0300

    init

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -7,31 +7,16 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace OnlineShopWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
+            var database = new ProductInitialization();
+            return  View(database.products);
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        }        
     }
 }
---
+++ OnlineShop/OnlineShopWebApp/Models/PoductInitialization.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Models
+{
+    public class ProductInitialization 
+    {
+        public List<Product> products => new List<Product>
+        {
+
+            new Product
+            { Id = 1, Name = "Ботинки",
+             Cost = 500,
+             Description = "Туристические ботинки для походов"
+            },
+            new Product
+            { Id = 2, Name = "Мужская обувь",
+             Cost = 300,
+             Description = "Повседневная обувь"
+            },
+            new Product
+            { Id = 3, Name = "Кроссовки",
+             Cost = 700,
+             Description = "Кроссовки для бега"
+            }
+
+        };
+
+
+
+
+
+
+
+
+        
+    }
+}
