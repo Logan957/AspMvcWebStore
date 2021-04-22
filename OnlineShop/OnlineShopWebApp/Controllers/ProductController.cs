aef6a8902a06915bb61ebe6307726ba0b5446da2
Logan957 <roman.zaidulin2015@gmail.com>
2021-04-22 09:58:40 +0300

    rename files

--- OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -10,17 +10,15 @@ using System.Threading.Tasks;
 
 namespace OnlineShopWebApp.Controllers
 {
-    public class HomeController : Controller
+    public class ProductController : Controller
     {
         public IActionResult Index(int? Id)
         {
             var database = new ProductInitialization();
-            if (Id != null)
-            {
-                var selectedProducts = database.products.Where(p => p.Id == Id);
-                return View(selectedProducts);
-            }
-            else { return View(database.products); }
+            var selectedProducts = database.products.Where(p => p.Id == Id);
+            return View(selectedProducts);
+            
+           
         }        
     }
 }
