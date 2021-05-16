b3b14e18f184d1901b5bb89a569cddf51d9c3553
Logan957 <roman.zaidulin2015@gmail.com>
2021-05-17 00:26:53 +0300

    Add Registration

---
+++ OnlineShop/OnlineShopWebApp/Controllers/RegistrationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registration(string login , string password , string confirmPassword) 
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
