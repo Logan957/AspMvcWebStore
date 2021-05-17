d297c2ac9ff4e1fd47891897e616c0df6a46da2a
Logan957 <roman.zaidulin2015@gmail.com>
2021-05-17 23:42:38 +0300

    refactor

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Login login , bool checkResponsible = false)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Register(Register register )
+        {
+            return RedirectToAction("Index", "Home");
+        }
+      
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
